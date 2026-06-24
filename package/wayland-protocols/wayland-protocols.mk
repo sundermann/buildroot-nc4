c96cec012bff6eb1d3b8f13757d9e6bda6fad761
Stephan Sundermann <stephansundermann@gmail.com>
2026-06-24 20:34:16 +0200

    package/wayland: pin versions for webOS compatibility

--- package/wayland-protocols/wayland-protocols.mk
+++ package/wayland-protocols/wayland-protocols.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-WAYLAND_PROTOCOLS_VERSION = 1.48
-WAYLAND_PROTOCOLS_SITE = https://gitlab.freedesktop.org/wayland/wayland-protocols/-/releases/$(WAYLAND_PROTOCOLS_VERSION)/downloads
+WAYLAND_PROTOCOLS_VERSION = 1.11
+WAYLAND_PROTOCOLS_SITE = http://wayland.freedesktop.org/releases
 WAYLAND_PROTOCOLS_SOURCE = wayland-protocols-$(WAYLAND_PROTOCOLS_VERSION).tar.xz
 WAYLAND_PROTOCOLS_LICENSE = MIT
 WAYLAND_PROTOCOLS_LICENSE_FILES = COPYING
@@ -14,6 +14,4 @@ WAYLAND_PROTOCOLS_INSTALL_TARGET = NO
 # needs wayland-scanner
 WAYLAND_PROTOCOLS_DEPENDENCIES = host-wayland
 
-WAYLAND_PROTOCOLS_CONF_OPTS = -Dtests=false
-
-$(eval $(meson-package))
+$(eval $(autotools-package))
--- package/wayland/wayland.mk
+++ package/wayland/wayland.mk
@@ -4,18 +4,21 @@
 #
 ################################################################################
 
-WAYLAND_VERSION = 1.24.0
-WAYLAND_SITE = https://gitlab.freedesktop.org/wayland/wayland/-/releases/$(WAYLAND_VERSION)/downloads
+WAYLAND_VERSION = 1.11.0
+WAYLAND_SITE = http://wayland.freedesktop.org/releases
 WAYLAND_SOURCE = wayland-$(WAYLAND_VERSION).tar.xz
 WAYLAND_LICENSE = MIT
 WAYLAND_LICENSE_FILES = COPYING
-WAYLAND_CPE_ID_VENDOR = wayland
 WAYLAND_INSTALL_STAGING = YES
 WAYLAND_DEPENDENCIES = host-pkgconf host-wayland expat libffi libxml2
+
+HOST_WAYLAND_VERSION = 1.15.0
+HOST_WAYLAND_SITE = http://wayland.freedesktop.org/releases
+HOST_WAYLAND_SOURCE = wayland-$(HOST_WAYLAND_VERSION).tar.xz
 HOST_WAYLAND_DEPENDENCIES = host-pkgconf host-expat host-libffi host-libxml2
 
-WAYLAND_CONF_OPTS = -Dtests=false -Ddocumentation=false
-HOST_WAYLAND_CONF_OPTS = -Dtests=false -Ddocumentation=false
+# wayland-scanner is only needed for building, not on the target
+WAYLAND_CONF_OPTS = --disable-scanner --with-host-scanner
 
 # Remove the DTD from the target, it's not needed at runtime
 define WAYLAND_TARGET_CLEANUP
@@ -23,17 +26,5 @@ define WAYLAND_TARGET_CLEANUP
 endef
 WAYLAND_POST_INSTALL_TARGET_HOOKS += WAYLAND_TARGET_CLEANUP
 
-# The wayland-scanner.pc installed by the target wayland package is
-# used to find the wayland-scanner tool, which in a cross-compilation
-# context is compiled for the host (and in Buildroot, compiled by
-# host-wayland). Below, we tweak the target wayland-scanner.pc so that
-# when the wayland_scanner variable is requested through pkg-config,
-# it points to the host wayland_scanner tool.
-define WAYLAND_TWEAK_WAYLAND_SCANNER_PATH
-	$(SED) 's%^wayland_scanner=.*%wayland_scanner=$(HOST_DIR)/bin/wayland-scanner%' \
-		$(STAGING_DIR)/usr/lib/pkgconfig/wayland-scanner.pc
-endef
-WAYLAND_POST_INSTALL_TARGET_HOOKS += WAYLAND_TWEAK_WAYLAND_SCANNER_PATH
-
-$(eval $(meson-package))
-$(eval $(host-meson-package))
+$(eval $(autotools-package))
+$(eval $(host-autotools-package))
