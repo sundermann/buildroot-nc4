441e854eaeb6044ede94b88578e6956058729d39
Stephan Sundermann <stephansundermann@gmail.com>
2026-06-24 20:24:03 +0200

    glibc: use openlgtv fork for legacy targets

--- package/glibc/glibc.mk
+++ package/glibc/glibc.mk
@@ -7,35 +7,25 @@
 # Generate version string using:
 #   git describe --match 'glibc-*' --abbrev=40 origin/release/MAJOR.MINOR/master | cut -d '-' -f 2-
 # When updating the version, please also update localedef
-GLIBC_VERSION = 2.44-36-g2d5421ffca8893534d5e02ad38c28acd8e778fa3
-GLIBC_SITE = https://gitlab.com/gnutools/glibc.git
-GLIBC_SITE_METHOD = git
+GLIBC_VERSION = fe819f0414e6385206dc8682c20d681ee0eb5998
 
-GLIBC_LICENSE = \
-	GPL-2.0+ (programs), \
-	LGPL-2.1+, BSD-2-Clause, BSD-3-Clause, BSL-1.0, FSFAP, ISC, other permissive licenses, public domain (library), \
-	LGPL-3.0+ (sysdeps/htl/raise.c, for Hurd only), \
-	GPL-3.0+ (scripts/move-if-change), \
-	GPL-3.0+ WITH Texinfo-exception (manual/texinfo.tex), \
-	GFDL-1.3-or-later (manual)
-GLIBC_LICENSE_FILES = COPYINGv2 COPYING.LESSERv2 COPYINGv3 LICENSES manual/fdl-1.3.texi
+# Upstream doesn't officially provide an https download link.
+# There is one (https://sourceware.org/git/glibc.git) but it's not reliable,
+# sometimes the connection times out. So use an unofficial github mirror.
+# When updating the version, check it on the official repository;
+# *NEVER* decide on a version string by looking at the mirror.
+# Then check that the mirror has been synced already (happens once a day.)
+GLIBC_SITE = $(call github,openlgtv,glibc,$(GLIBC_VERSION))
+
+#BR_NO_CHECK_HASH_FOR += $(GLIBC_SOURCE)
+
+GLIBC_LICENSE = GPL-2.0+ (programs), LGPL-2.1+, BSD-3-Clause, MIT (library)
+GLIBC_LICENSE_FILES = COPYING COPYING.LIB LICENSES
 GLIBC_CPE_ID_VENDOR = gnu
 
 # Extract the base version (e.g. 2.38) from GLIBC_VERSION in order to
 # allow proper matching with the CPE database.
 GLIBC_CPE_ID_VERSION = $(word 1, $(subst -,$(space),$(GLIBC_VERSION)))
-
-# Fixed by 2.44-26-gd6ff274313d79feb864cc10eb775b91c817a67e9
-GLIBC_IGNORE_CVES += CVE-2026-19542
-
-# Fixed by 2.44-29-g63b53df549451a5d69fcba6d7612ea99f517e8e3
-GLIBC_IGNORE_CVES += CVE-2026-19499
-
-# Fixed by 2.44-30-g6f9b2bfa500bf5d1cff5d990adfff4b71298dadd
-GLIBC_IGNORE_CVES += CVE-2026-77117
-
-# Fixed by 2.44-31-gcb61572ea3f773e1e1978f6c412cc36a30acdb0c
-GLIBC_IGNORE_CVES += CVE-2026-80489
 
 # This CVE is considered as not being security issues by
 # upstream glibc:
@@ -53,6 +43,8 @@ GLIBC_DEPENDENCIES = host-gcc-initial linux-headers host-bison host-gawk \
 GLIBC_SUBDIR = build
 
 GLIBC_INSTALL_STAGING = YES
+
+GLIBC_INSTALL_STAGING_OPTS = install_root=$(STAGING_DIR) install
 
 # Thumb build is broken, build in ARM mode
 ifeq ($(BR2_ARM_INSTRUCTIONS_THUMB),y)
@@ -92,7 +84,14 @@ endif
 GLIBC_CONF_ENV = \
 	ac_cv_path_BASH_SHELL=/bin/$(if $(BR2_PACKAGE_BASH),bash,sh) \
 	libc_cv_forced_unwind=yes \
+	libc_cv_c_cleanup=yes \
 	libc_cv_ssp=no
+
+# Don't use webOS compatibility hacks
+ifeq ($(BR2_PACKAGE_LGTV),y)
+# Ugly hack to modify CC (because CFLAGS isn't used everywhere we need)
+GLIBC_CONF_ENV += CC="$(TARGET_CC) -tno-lgtv-compat"
+endif
 
 # POSIX shell does not support localization, so remove the corresponding
 # syntax from ldd if bash is not selected.
@@ -144,6 +143,8 @@ ifeq ($(BR2_OPTIMIZE_FAST),y)
 GLIBC_CFLAGS += -O2
 endif
 
+GLIBC_CFLAGS = -O2
+
 define GLIBC_CONFIGURE_CMDS
 	mkdir -p $(@D)/build
 	# Do the configuration
@@ -163,8 +164,9 @@ define GLIBC_CONFIGURE_CMDS
 		--disable-profile \
 		--disable-werror \
 		--without-gd \
+		--enable-obsolete-rpc \
 		--with-headers=$(STAGING_DIR)/usr/include \
-		$(if $(BR2_aarch64)$(BR2_aarch64_be),--enable-mathvec) \
+		--enable-add-ons=nptl,ports \
 		$(GLIBC_CONF_OPTS))
 	$(GLIBC_ADD_MISSING_STUB_H)
 endef
